db916ec8253ee7c7d263fbd35b4f6612a49efe88
Rui Pedro <rlopes@ipb.pt>
2023-01-14 18:46:07 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/EndPointController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import io.grpc.StatusRuntimeException;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/grpc")
+public class EndPointController {
+
+    @GrpcClient("grpc-model")
+    private SimpleGrpc.SimpleBlockingStub simpleBlockingStub;
+
+    @GetMapping("/{name}")
+    public String helloWorld(@PathVariable String name) {
+        try {
+            final HelloReply response = this.simpleBlockingStub.sayHello(HelloRequest.newBuilder().setName(name).build());
+            return response.getMessage();
+        } catch (final StatusRuntimeException e) {
+            return "FAILED with " + e.getStatus().getCode().name();
+        }
+    }
+
+    @GetMapping
+    public String ping() {
+        return "pong";
+    }
+
+}
